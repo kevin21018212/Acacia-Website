de0b780a07d4fb5b85ec4ff5d262d2e3d42161ce
kevin21018212 <mmb2003@iastate.edu>
2024-01-01 14:38:07 -0600

    better images

--- app/memberdata.ts
+++ app/memberdata.ts
@@ -3,31 +3,31 @@ const boardMembersData = [
     name: "James A. Davis",
     position: "President & Founding Director",
     bio: "My commitment and service to ISACF are my way of repaying, at least in part, what ISU Acacia did for me. I was just a green Iowa farm kid back in my undergraduate college days, and it's been 65 years since then.",
-    image: "/images/jimdavis.jpeg", // Use full path from the root
+    image: "/images/jimdavis.jpg", // Use full path from the root
   },
   {
     name: "Roger Kingery",
     position: "Vice-President",
     bio: "I took the position on the board to try to help and support the chapter to provide the positive influence on its members that I enjoyed in my undergraduate time.",
-    image: "/images/rogerkingrey.jpeg", // Use full path from the root
+    image: "/images/rogerkingery.jpg", // Use full path from the root
   },
   {
     name: "Harold Zarr",
     position: "Secretary and Treasurer",
     bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-    image: "/images/haroldzarr.jpeg", // Use full path from the root
+    image: "/images/haroldzarr.jpg", // Use full path from the root
   },
   {
     name: "John Lynch",
     position: "Member",
     bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-    image: "/images/johntynch.jpg", // Use full path from the root
+    image: "/images/johntynch.jpg",
   },
   {
     name: "Craig Buske",
     position: "Member",
     bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-    image: "/images/buskecraig.jpeg", // Use full path from the root
+    image: "/images/buskecraig.jpg",
   },
 
   {
