3064ad823be14c5a2e4507075dfd86a5edf1787d
kevin21018212 <mmb2003@iastate.edu>
2024-01-01 13:52:04 -0600

    members info and css changes

--- app/memberdata.ts
+++ app/memberdata.ts
@@ -2,13 +2,13 @@ const boardMembersData = [
   {
     name: "James A. Davis",
     position: "President & Founding Director",
-    bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    bio: "My commitment and service to ISACF are my way of repaying, at least in part, what ISU Acacia did for me. I was just a green Iowa farm kid back in my undergraduate college days, and it's been 65 years since then.",
     image: "/images/jimdavis.jpeg", // Use full path from the root
   },
   {
     name: "Roger Kingery",
     position: "Vice-President",
-    bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    bio: "I took the position on the board to try to help and support the chapter to provide the positive influence on its members that I enjoyed in my undergraduate time.",
     image: "/images/rogerkingrey.jpeg", // Use full path from the root
   },
   {
